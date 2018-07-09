339a52175d552e25ffd53fb7cd9badc32a2d82b1
Manikiran-M <40986487+Manikiran-M@users.noreply.github.com>
2018-07-09 00:54:19 -0700

    Add files via upload

---
+++ Foreven.java
@@ -0,0 +1,16 @@
+/* Java program to print even numbers using for loop
+
+class Foreven {
+public static void main(String args[])
+{
+for(int number=0;number<=10;number++)
+{
+if(number%2==0)
+{
+System.out.println(number);
+}
+}
+}
+}
+
+*/
