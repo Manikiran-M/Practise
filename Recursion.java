8a3b64f4ef6e57acc21da707fd0c1d366952d9d6
Manikiran-M <40986487+Manikiran-M@users.noreply.github.com>
2018-07-09 00:56:57 -0700

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,14 @@
+class Recursion {
+static int rec(int n) {
+for(int i=0;i<=n;i++)
+{
+System.out.println(i);
+}
+return n;
+
+}
+public static void main(String args[])
+{
+rec(10);
+}
+}
---
+++ Switchcase.java
@@ -0,0 +1,11 @@
+class Switchcase {
+public static void main(String args[])
+{
+String name="Dhoni";
+switch(name) {
+case "Kohli" : System.out.print("Current Indian captain");break;
+case "Dhoni": System.out.print("Former Indian Captain");break;
+case "Rohit": System.out.print("Double century hero");break;
+}
+}
+}
