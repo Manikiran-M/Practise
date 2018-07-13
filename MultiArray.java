4b604a0b595485d45fdfc87b35f96f02347a59c5
Manikiran-M <40986487+Manikiran-M@users.noreply.github.com>
2018-07-13 05:50:28 -0700

    Update MultiArray.java

--- MultiArray.java
+++ MultiArray.java
@@ -1,12 +1,12 @@
 class MultiArray {
 	public static void main(String args[])
 		{
-		int a[][]={{1,2,3},{2,4,5},{6,7,8}};
+		int Matrix[][]={{1,2,3},{2,4,5},{6,7,8}};
 		for(int i=0;i<3;i++)
 			{
 			for(int j=0;j<3;j++)
 				{
-				System.out.print(a[i][j]+" ");
+				System.out.print(Matrix[i][j]+" ");
 				}
 				System.out.println();
 			}
