738c26a79c29b640edb5c7b0f9e6fb852a510b35
Manikiran-M <40986487+Manikiran-M@users.noreply.github.com>
2018-07-13 05:49:27 -0700

    Update AdditionMatrix.java

--- AdditionMatrix.java
+++ AdditionMatrix.java
@@ -3,13 +3,13 @@ class AdditionMatrix
 {
 	public static void main(String args[])
 	{
-	int a[][]={{1,4,7},{2,5,8},{3,6,9}};
-	int b[][]={{3,6,9},{2,5,8},{1,4,7}};
+	int Matrix1[][]={{1,4,7},{2,5,8},{3,6,9}};
+	int Matrix2[][]={{3,6,9},{2,5,8},{1,4,7}};
 	for(int i=0;i<3;i++)
 		{
 		for(int j=0;j<3;j++)
 			{
-			System.out.print(a[i][j]+b[i][j]+" ");
+			System.out.print(Matrix1[i][j]+Matrix2[i][j]+" ");
 			}
 		System.out.println();
 		}
